668a9d61a443e84c796e8d9cb0cbbfcff82ac652
sushil-mahto <sushilkr561998@gmail.com>
2022-02-18 15:23:31 +0530

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by SUshil on 18/02/22.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
