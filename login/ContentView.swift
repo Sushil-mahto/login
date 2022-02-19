1a6e1dbd702fef7ae20e928d5e69bde77c100aff
sushil-mahto <sushilkr561998@gmail.com>
2022-02-19 12:28:06 +0530

    login

--- login/ContentView.swift
+++ login/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = "sushil@gmail.com"
+    @State var pass = ""
+    @State var loginStatus = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if (!loginStatus){
+                Login(email: $email, pass: $pass)
+            }else{
+                NavUIView()
+            }
+        }
+    }
+}
+//login page
+struct Login: View{
+    @Binding var email:String
+    @Binding var pass:String
+    
+    @State var prst = false
+    @AppStorage("status") var loginStatus = false
+    
+    let tempemail = "sushil@gmail.com"
+    let temppass = "123456"
+    
+    var body: some View{
+        VStack{
+            Text("Login")
+                .font(.largeTitle)
+                .bold()
+            TextField("Email",text:$email)
+                .padding()
+                .background(.gray)
+                .padding(.bottom,10)
+            
+            TextField("password",text:$pass)
+                .padding()
+                .background(.gray)
+                .padding(.bottom,10)
+            Text("LogIn")
+                .padding()
+                .foregroundColor(Color.white)
+                .background(.blue)
+                .cornerRadius(10)
+                .onTapGesture {
+                    if(email == tempemail && pass == temppass){
+                        print("yes")
+                        loginStatus.toggle()
+                    }else{
+                        print("fail")
+                        prst.toggle()
+                    }
+                }
+            
+            /*Button(action: {
+                if(email == tempemail && pass == temppass){
+                    print("yes")
+                    loginStatus.toggle()
+                }else{
+                    print("fail")
+                    prst.toggle()
+                }
+            }, label: {
+                Text("Submit")
+                    .font(.largeTitle)
+            })*/
+            Spacer()
+        }
+        .alert(isPresented: $prst, content:{
+            Alert(title:Text( "incorrect"),message:Text("you have entered wrong password"),dismissButton:.default(Text("OK")))
+            
+        })
     }
 }
 
---
+++ login/NavUIView.swift
@@ -0,0 +1,20 @@
+//
+//  NavUIView.swift
+//  login
+//
+//  Created by SUshil on 19/02/22.
+//
+
+import SwiftUI
+
+struct NavUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavUIView()
+    }
+}
